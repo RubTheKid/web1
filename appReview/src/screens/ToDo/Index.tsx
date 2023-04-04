5ee79b2f5cfca340136d20079f6d154b1ec00879
RubTheKid <jadercardoso@tuta.io>
2023-04-03 22:36:47 -0300

    update

--- appReview/src/screens/ToDo/Index.tsx
+++ appReview/src/screens/ToDo/Index.tsx
@@ -1,17 +1,151 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { AntDesign } from '@expo/vector-icons'; 
+import { StyleSheet,TextInput, TouchableOpacity, Text, View, FlatList, SafeAreaView } from 'react-native';
+import { AntDesign,  Entypo , Ionicons, FontAwesome5  } from '@expo/vector-icons'; 
+// import { Icon } from 'react-native-ionicons';
 
 import { styles } from './Styles'
+import React from 'react';
+
+
 
 
 export default function ToDo(){
+
+    interface DataItem {
+        text: string;
+        isDone: number;
+      }
+
+      interface ItemProps {
+        item: DataItem;
+      }
+
+    const DATA = [
+        {
+          text: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+          isDone: 1,
+        },
+        {
+            text: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+            isDone: 0,
+        },
+        {
+            text: '58694a0f-3da1-471f-bd96-145571e29d72',
+            isDone: 0,
+        },
+      ];
+
+     
+
+      const renderItem = ({ item }: ItemProps) => (
+        <View style={styles.itemBox}>
+          <Text>{item.text}</Text>
+          <Text>{item.isDone}</Text>
+        </View>
+      );
+      
+      
     return(
+        
+
+        <>
+        <View style={styles.body}>
         <View style={styles.header}>
-            <AntDesign name="bars" size={36} color="black" alignItems='center' >
-                <Text style={{color:'#4B0082', fontSize:40}}>to</Text>
-                <Text style={{color:'#FF69B4', fontSize:40 }}>do</Text>
+            <AntDesign name="bars" size={36} color="black" alignItems='center'>
+                <Text style={styles.header.to}>to</Text>
+                <Text style={styles.header.do}>do</Text>
             </AntDesign>
         </View>
-    )
-}
+        
+        </View>
+        
+        <View style={styles.newTask}>
+        <TextInput
+        style={styles.newTask.Input}
+        placeholder="Adicione uma Tarefa"
+        placeholderTextColor={"#6d6d6d"}
+        autoCapitalize="characters" 
+        />
+                     <TouchableOpacity style={styles.newTask.addBtn}>
+                     <Ionicons name="add-circle-outline" />
+          </TouchableOpacity>
+        </View>
+       
+        <View style={styles.tasks}>
+                <Text style={styles.tasks.created}>Criadas: <Text style = {styles.tasks.created.total}>5 </Text></Text>
+                
+                <Text style={styles.tasks.done}>Completas: <Text style={styles.tasks.done.total}>2</Text></Text>
+                
+
+        </View>
+
+        <FlatList
+      data={DATA}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.text}
+      
+    />
+  
+            
+            </>
+     
+        )};
+    // } <View style={styles.taskInput}>
+    //             <TextInput
+    //                 style={styles.taskInput.taskText}
+    //                 placeholder="Adicione uma Tarefa"
+    //                 placeholderTextColor={"#6d6d6d"}
+    //                 autoCapitalize="characters" 
+    //                 />
+    //             <TouchableOpacity>
+    //                 <Ionicons name="add-circle-outline" size={24} color="white" />
+    //             </TouchableOpacity>
+    //         </View>
+            
+            
+            
+        // <View style={styles.taskInput}>
+            
+        /* <TextInput
+            style={styles.taskInput.taskText}
+            placeholder="Adicione uma Tarefa"
+            placeholderTextColor={"#6d6d6d"}
+            autoCapitalize="characters"
+          /> */
+
+
+
+/* 
+
+          <TouchableOpacity >
+          <Ionicons name="add-circle-outline" size={24} color="white" />
+          </TouchableOpacity>
+
+        </View>
+   <View>    
+        <Text>Criadas: 5</Text>
+    <Text>Concluidas: 2</Text> */
+
+    
+
+    /* <View style={styles.itemDiv}>
+      <View style={styles.touckBgd}>
+    
+      <Entypo name="circle" size={24} color="purple"/>
+    
+        </View>
+
+        <Text
+            style={styles.input}
+            placeholderTextColor={"#6d6d6d"}
+            autoCapitalize="characters"
+          >
+          aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        </Text>
+        <Ionicons name="md-trash-outline" size={24} color="red" />
+
+
+        </View>
+</View> */
+       
+
 
--- appReview/src/screens/ToDo/Styles.ts
+++ appReview/src/screens/ToDo/Styles.ts
@@ -1,13 +1,166 @@
-
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 
 export const styles =  StyleSheet.create({
+
+    body: {
+      flexDirection: "row",
+      justifyContent: "center", 
+      // backgroundColor: '#FAEBD7',
+    },
     header: {
       flex: 1,
-      paddingTop: 54,
       alignItems: 'center',
-      backgroundColor: '#FAEBD7'
+      marginTop:100,
+      backgroundColor:'red',
+      to:{
+        color:'#4B0082', fontSize:40
+      },
+      do:{
+        color:'#FF69B4', fontSize:40
+      }
+     },
+     newTask:{
+    flexDirection:'row',
+    paddingTop:20,
+    backgroundColor:'purple',
+    Input:{
+    backgroundColor:'#1F1E25',
+    flex: 0,
+    gap: 5,
+    borderRadius: 5,
+    width: 300,
+    height:50,
+    marginLeft:30,
+    marginBottom:20, 
+     },
+     addBtn:{ 
+          backgroundColor: '#b4b4b4',
+          borderRadius: 4,
+          height: 50,
+          width: 50,
+          justifyContent: 'center',
+          alignItems: 'center',
+      paddingRight:0,
     },
-  });
+ },
+
+    tasks:{
+      backgroundColor:'yellow',
+      flexDirection:'row',
+      justifyContent: 'space-between',
+      paddingBottom: 10,
+      paddingTop:10,
+      created:{
+        marginLeft:50,
+        fontWeight:'bold',
+        total:{
+          color: 'red'
+        },
+      },
+      done:{
+        marginRight: 50,
+        fontWeight:'bold',
+        total:{
+         color:'red' 
+        }
+      },
+    
+     
+    },
+    flatList:{
+      backgroundColor:'navy'
+     },
+     
+   // width: 300,
+    // padding: 18,
+    // backgroundColor: "#1F1E25",
+    // borderRadius: 4,
+    // color: "#fff",
+    // fontSize: 18,
+
+      // paddingTop: 54,
+      // paddingBottom: 200,
+      // paddingLeft: 60,
+      // paddingRight: 60,
+    
+   itemBox:{
+    backgroundColor: 'red',
+    paddingBottom: 30,
+    paddingLeft: 15,
+    borderRadius:10,
+    marginLeft:30,
+    marginTop:30
+   }
+  
+
+});
+
+// taskInput: {
+//       justifyContent: "center",
+//       alignItems: "center",
+//       flexDirection: "row",
+//       taskText:{
+//         borderRadius: 4,
+//       width: 200,
+//       height: 40,
+//       borderWidth: 1,
+//       padding: 10,
+//       backgroundColor: "#6d6d6d",
+//       },
+//       addBtn: {
+//           justifyContent: "center",
+//           alignItems: "center",
+//           flexDirection: "row",
+//           backgroundColor: "blue",
+//           borderRadius: 4,
+//           height: 62,
+//           width: 60,
+  //   container: {
+  //     paddingTop: 70,
+  //     paddingBottom: 200,
+  //     paddingLeft: 60,
+  //     paddingRight: 60,
+  //   },
+  // //   },
+  // //   inputDiv: {
+  // //   alignItems: "center",
+  // //   justifyContent: "center",
+  // //   flex: 0,
+  // //   flexDirection: "row",
+  // //   backgroundColor: "#1F1E25",
+  // //   gap: 5,
+  // //   borderRadius: 5,
+    
+  // // },
+  // // addInput: {
+  // //   width: 300,
+  // //   padding: 18,
+  // //   backgroundColor: "#1F1E25",
+  // //   borderRadius: 4,
+  // //   color: "#fff",
+  // //   fontSize: 18,
+  // // },
+  // // 
+    
+  // // },
+  // // itemDiv: {
+  // //   alignItems: "center",
+  // //   justifyContent: "center",
+  // //   flex: 0,
+  // //   flexDirection: "row",
+  // //   gap: 5
+  // // },
+  // // touckBgd: {
+  // //   color: '#FFF',
+  // // },
+  // // input: {
+  // //   width: 300,
+  // //   padding: 18,
+  // //   backgroundColor: "#1F1E25",
+  // //   borderRadius: 4,
+  // //   color: "#fff",
+  // //   fontSize: 18,
+  // // }
+
   
